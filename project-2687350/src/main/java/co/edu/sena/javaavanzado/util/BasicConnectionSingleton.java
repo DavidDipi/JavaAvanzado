61a7bc205cc27f4627e5522e85016e899b1f7074
DavidDipi <85139022+DavidDipi@users.noreply.github.com>
2023-05-09 19:51:45 -0500

    [Taller 6]

--- project-2687350/src/main/java/co/edu/sena/javaavanzado/util/BasicConnectionSingleton.java
+++ project-2687350/src/main/java/co/edu/sena/javaavanzado/util/BasicConnectionSingleton.java
@@ -3,8 +3,7 @@ package co.edu.sena.javaavanzado.util;
 import java.sql.*;
 
 public class BasicConnectionSingleton {
-    private static String url =
-            "jdbc:mysql://localhost:3306/my_app?serverTimezone=America/Bogota";
+    private static String url = "jdbc:mysql://localhost:3306/my_app?serverTimezone=America/Bogota";
     private static String user = "myapp";
     private static String pass = "d0Tio0jCk1365";
     private static Connection conn;
